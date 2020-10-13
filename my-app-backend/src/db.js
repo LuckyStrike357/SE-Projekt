2dfd0fa811a12a7b304dcdc24ffcf13bdb0a3a83
Mona Kaczun <mona.kaczun@sap.com>
2020-10-13 14:30:45 +0200

    change db from mysql to mariadb

---
+++ my-app-backend/src/db.js
@@ -0,0 +1,22 @@
+const mariadb = require('mariadb');
+const pool = mariadb.createPool(
+  {
+    host: "localhost",
+    user: "Admin",
+    password: "admin123456",
+    database: 'test_db'
+  }
+);
+
+// expose the ability to create new connections
+module.exports={
+    getConnection: function(){
+      return new Promise(function(resolve,reject){
+        pool.getConnection().then(function(connection){
+          resolve(connection);
+        }).catch(function(error){
+          reject(error);
+        });
+      });
+    }
+  } 
--- my-app-backend/src/server.js
+++ my-app-backend/src/server.js
@@ -9,15 +9,41 @@ app.use(bodyParser.json());
 app.get('/hello', (req,res) =>res.send('Hello!'));
 app.post('/hello', (req,res)=> res.send(`Hello ${req.body.name}!`));
 
+const pool = require('./db');
+const port = 8000;
 
-const mysql = require('mysql');
+// expose an endpoint
+app.get('/testdb', async (req, res) => {
+  let conn;
+  try {
+      // establish a connection to MariaDB
+      conn = await pool.getConnection();
 
+      // create a new query
+      var query = "select * from pet";
+
+      // execute the query and set the result to a new variable
+      var rows = await conn.query(query);
+
+      // return the results
+      res.send(rows);
+      // res.status(200).send(rows);
+  } catch (err) {
+      throw err;
+  } finally {
+      if (conn) return conn.release();
+  }
+});
+
+//const mysql = require('mysql');
+
+/*
 const con = mysql.createConnection({
   host: "localhost",
   user: "Admin",
   password: "admin123456",
   database: 'test'
-});
+});*/
 
 /*
 con.connect(function(err) {
@@ -29,12 +55,30 @@ con.connect(function(err) {
   });
 });*/
 
+
+app.get('testdb', (req,res)=>{
+  async function asyncFunction() {
+    let conn;
+    try {
+    conn = await pool.getConnection();
+    const rows = await conn.query("SELECT * FROM pet");
+    console.log(rows); //[ {val: 1}, meta: ... ]
+    res.status(200).send(rows);
+    } catch (err) {
+    throw err;
+    } finally {
+    if (conn) return conn.end();
+    }
+  }
+});
+/*
 app.get('/testdb',(req,res) =>{
+  const con = pool.getConnection();
   con.query("SELECT * FROM pet", function (err, result, fields) {
     if (err) throw err;
     console.log(result);
     res.status(200).send(result);
-  });   
+  });   */
 /*con.connect(function(err) {
     if (err) throw err;
     console.log("Connected!");
@@ -44,7 +88,7 @@ app.get('/testdb',(req,res) =>{
         res.status(200).send(result);
       });
 });*/
-});
+//});
 
 app.get('*',(req,res) => {
   res.sendFile(path.join(__dirname + '/build/index.html'));
