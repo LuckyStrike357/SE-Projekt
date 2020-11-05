e4cd194a73cbff701f843a88e4e3dd33eb8b2668
Mona Kaczun <mona.kaczun@sap.com>
2020-11-05 13:22:32 +0100

    Update DataFormPage

--- my-app/src/pages/DataFormPage.js
+++ my-app/src/pages/DataFormPage.js
@@ -1,25 +1,45 @@
-import React, { useState,useEffect, Component } from 'react';
+import React, { useState, useEffect, Component } from 'react';
 
 import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 import history from './../history';
 
-export default class DataFormPage extends Component{
+export default class DataFormPage extends Component {
     state = {
         bookingCode: new String(),
-      }
+    }
 
-    onClickNext = () =>{
+    onClickNext = () => {
         //test if booking has capacity
         console.log(this)
-        history.push({ pathname:'/bookingCode', data: this.state.bookingCode});
-        
+        history.push({ pathname: '/bookingCode', data: this.state.bookingCode });
+
     }
     render() {
         return (
-        <React.Fragment>
-        <h1>DataFormPage Page!{console.log(this.props.location.data)}</h1>
-        <Button variant="primary" onClick={()=>this.onClickNext()}>Next</Button>
-        </React.Fragment>
+            <React.Fragment>
+                <h1>DataFormPage Page!{console.log(this.props.location.data)}</h1>
+                <div className="FormPersonalInformation">
+                <Form>
+                    <Form.Group controlId="formBasicEmail">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control type="email" placeholder="Enter email" />
+                        <Form.Text className="text-muted">
+                            We'll never share your email with anyone else.</Form.Text>
+                    </Form.Group>
+
+                    <Form.Group controlId="formBasicPassword">
+                        <Form.Label>Name</Form.Label>
+                        <Form.Control type="password" placeholder="Password" />
+                    </Form.Group>
+                    <Form.Group controlId="formBasicCheckbox">
+                        <Form.Check type="checkbox" label="Check me out" />
+                    </Form.Group>
+                    <Button variant="primary" type="submit">Submit</Button>
+                </Form>
+                </div>
+                <Button variant="primary" onClick={() => this.onClickNext()}>Next</Button>
+            </React.Fragment>
         );
     }
 }
--- my-app/src/pages/FindSlotPage.js
+++ my-app/src/pages/FindSlotPage.js
@@ -3,19 +3,22 @@ import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 
 import Button from 'react-bootstrap/Button';
+import Table from 'react-bootstrap/Table';
 import history from './../history';
 
 export default class FindSlortPage extends Component{
     state = {
         date: new Date(),
-        booking: new Date()
+        booking: new Date(),
+        timeslots: new Array(),
+        bookings: new Array(),
       }
      
       onChange = (date) => {
           this.setState({ date });
           console.log("state", date);
           console.log("state date",this.state.date);
-          console.log("state boooking",this.state.booking);
+          console.log("state boooking",this.state.bookings);
           //fetch Available slots for day
           this.fetchBookings();
           this.fetchTimeslots();
@@ -25,13 +28,15 @@ export default class FindSlortPage extends Component{
           console.log("start fetch data");
         const result = await fetch (`/bookings`);
         const body = await result.json();
+        this.setState({bookings: body});
         console.log(result);
         console.log(body);
     }
     fetchTimeslots = async () => {
         console.log("start fetch data");
-      const result = await fetch (`/bookings`); //timeslots is not existant jet
+      const result = await fetch (`/timeslots`); //timeslots is not existant jet
       const body = await result.json();
+      this.setState({timeslots: body});
       console.log(result);
       console.log(body);
     }
@@ -42,6 +47,27 @@ export default class FindSlortPage extends Component{
         history.push({ pathname:'/dataForm', data: this.state.booking});
         
     }
+
+    renderTableData=() =>{
+        return this.state.timeslots.map((timeslot, index) => {
+           const { id, start, end, capacity, bookings } = timeslot //destructuring
+           console.log(timeslot)
+           if (bookings != undefined){
+           console.log(bookings);
+           const free = 10;//capacity - bookings.length;
+           return (
+              <tr key={id}>
+                 <td>{start}</td>
+                 <td>{end}</td>
+                 <td>{capacity}</td>
+                 <td>{free}</td>
+              </tr>
+           )
+           }else{
+
+           }
+        })
+     }
      
       render() {
         return (
@@ -55,7 +81,19 @@ export default class FindSlortPage extends Component{
             </div>
             <div className="findTime">
             <h1>Find Slot on {this.state.date.toLocaleDateString()}</h1>
-
+            <Table striped bordered hover>
+                <thead>
+                    <tr>
+                    <th>Start</th>
+                    <th>End</th>
+                    <th>Capacity</th>
+                    <th>Bookings</th>
+                    </tr>
+                </thead>
+                <tbody>
+                {this.renderTableData()}
+                </tbody>
+            </Table>
             </div>
             <Button variant="primary" onClick={()=>this.onClickNext()}>Next</Button>
             </div>
