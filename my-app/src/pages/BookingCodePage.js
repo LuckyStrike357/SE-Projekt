190a0590f1d138650d1e5e7369ccca38549e2adf
Dustin Ramseger <dustin.ramseger@sap.com>
2020-11-06 16:49:00 +0100

    Simple booking code page displaying a qr code

--- my-app/src/pages/BookingCodePage.js
+++ my-app/src/pages/BookingCodePage.js
@@ -1,7 +1,45 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
+import QRCode from 'qrcode.react';
+import Button from 'react-bootstrap/Button';
+import history from './../history';
 
-const BookingCodePage = () => (
-    <h1>BookingCode</h1>
-);
+export default class BookingCodePage extends Component {
 
-export default BookingCodePage;
+    state = {
+
+    }
+
+    getDetails() {
+
+    }
+
+    render() {
+        return (
+            <React.Fragment>
+                <h1>Booking Code Page!</h1>
+                <div className="textboxes">
+
+                    <QRCode
+                        value={"http://picturesofpeoplescanningqrcodes.tumblr.com/"}
+                        size={128}
+                        bgColor={"#ffffff"}
+                        fgColor={"#000000"}
+                        level={"L"}
+                        includeMargin={false}
+                        renderAs={"svg"}
+                        imageSettings={{
+                            src: "https://static.zpao.com/favicon.png",
+                            x: null,
+                            y: null,
+                            height: 24,
+                            width: 24,
+                            excavate: true,
+                        }}
+                    />
+                </div>
+                <Button variant="primary" onClick={() => history.push({ pathname: '/' })}>Home</Button>
+
+            </React.Fragment >
+        );
+    }
+}
