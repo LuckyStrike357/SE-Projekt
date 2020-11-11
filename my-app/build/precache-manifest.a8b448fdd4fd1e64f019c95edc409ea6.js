76171d385e62ffd040d1a717e79d31657ff36115
DoelliJ <48750590+DoelliJ@users.noreply.github.com>
2020-11-11 14:33:10 +0100

    DataFormPage Error Messages Update

---
+++ my-app/build/precache-manifest.a8b448fdd4fd1e64f019c95edc409ea6.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cbd78c77032f6df2523d458500e2e0b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c7da3d6b2bd30824a061",
+    "url": "/static/css/2.672dc553.chunk.css"
+  },
+  {
+    "revision": "2d5c6a47b5f398bd249d",
+    "url": "/static/css/main.3f00713f.chunk.css"
+  },
+  {
+    "revision": "c7da3d6b2bd30824a061",
+    "url": "/static/js/2.29d64089.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.29d64089.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d5c6a47b5f398bd249d",
+    "url": "/static/js/main.102195ac.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "cb7a850d82e9bb099b1b994c7d64b4f0",
+    "url": "/static/media/CreteRound-Regular.cb7a850d.ttf"
+  },
+  {
+    "revision": "bdf39146b211608463203da62cf55411",
+    "url": "/static/media/Mundschutz.bdf39146.jpg"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "d803ade284becd9467ee312b824fcd92",
+    "url": "/static/media/papers.d803ade2.jpg"
+  },
+  {
+    "revision": "c66c39a16369323a93d306711e9f73a8",
+    "url": "/static/media/swimmer.c66c39a1.jpg"
+  }
+]);
--- my-app/src/pages/StartPage.js
+++ my-app/src/pages/StartPage.js
@@ -4,20 +4,26 @@ import CardColumns from 'react-bootstrap/CardColumns';
 import CardDeck from 'react-bootstrap/CardDeck';
 import Button from 'react-bootstrap/Button';
 import Badge from 'react-bootstrap/Badge';
+import { Form, FormRow, Col, FormLabel, FormGrid, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
 
 
 import history from './../history';
 import logo from './../images/logo.svg';
 import swimmer from './../images/swimmer.jpg';
-import paper from './../images/papers.jpg';
+import bin from './../images/bin.png';
 
 
 const StartPage = () => (
 
   <div class="CardDeckComponent">
-    <div class="Header">
-      Willkommen!
-    </div>
+    <Form.Row>
+      <Form.Group as={Col} controlId="lögö" id="lögö">
+        <img src="./../H20_Logo3.jpg" id= "logo" width="100" height="80"/>
+      </Form.Group>
+      <Form.Group as={Col} controlId="streks" id="streks">
+        <h1 id="StartHeader">Willkommen!</h1>
+    </Form.Group>
+    </Form.Row>
     <div class="SubHeader">
       Bitte wählen Sie eine Option, mit der Sie fortfahren möchten
     </div>
@@ -30,27 +36,27 @@ const StartPage = () => (
             <Card.Title>Neue Buchung</Card.Title>
             <Card.Text>
 
-              Some quick example text to build on the card title and make up the bulk of
-              the card's content. Test.
+              Wenn Sie eine neue Buchung für das Schwimmbad tätigen möchten, klicken Sie auf den unten stehenden Button. 
+              
             </Card.Text>
 
-            <Button variant="primary" onClick={() => history.push('/findSlot')}>Find a Slot!</Button>
+            <Button variant="primary" onClick={() => history.push('/findSlot')}>Jetzt Platz buchen!</Button>
           </Card.Body>
         </Card>
         <Card>
-          <Card.Img variant="top" src={paper} />
+          <Card.Img variant="top" src={bin} />
           <Card.Body>
             <Card.Title>Bestehende Buchung löschen</Card.Title>
             <Card.Text>
               Sie haben ihre Pläne verworfen und möchten eine bereits getätigte Buchung löschen? Dann folgen Sie diesem Button.
         </Card.Text>
-            <Button variant="primary" onClick={() => history.push('/cancle')}>Go To Cancle Booking</Button>
+            <Button variant="primary" onClick={() => history.push('/cancle')}>Buchung löschen</Button>
           </Card.Body>
         </Card>
       </CardDeck>
 
-    <hr2>
-
+    <hr2 id="impressum">
+      Impressum: <br/>
        Tel: +49931/652781 <br/>
        E-Mail: bade.meister@swimmingmail.com <br/>
        Adresse: Wasserstraße 12
