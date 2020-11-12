26dc57d7972cc5f09538792e418c6a36bbe491df
root <root@ip-172-31-36-88.eu-central-1.compute.internal>
2020-11-12 11:52:47 +0000

    test

---
+++ my-app-backend/src/build/precache-manifest.9d09203f76d261afc9a247d7ffbacebb.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c7455ce8d404d6d75342d7036eabec9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c3c07d99cd9ee2660854",
+    "url": "/static/css/2.672dc553.chunk.css"
+  },
+  {
+    "revision": "28f755f98d3b1670057c",
+    "url": "/static/css/main.1a1cd839.chunk.css"
+  },
+  {
+    "revision": "c3c07d99cd9ee2660854",
+    "url": "/static/js/2.db042d06.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.db042d06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28f755f98d3b1670057c",
+    "url": "/static/js/main.dcf5a812.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "cb7a850d82e9bb099b1b994c7d64b4f0",
+    "url": "/static/media/CreteRound-Regular.cb7a850d.ttf"
+  },
+  {
+    "revision": "bdf39146b211608463203da62cf55411",
+    "url": "/static/media/Mundschutz.bdf39146.jpg"
+  },
+  {
+    "revision": "82f6028c64b7335f7c63fe9e194adfa7",
+    "url": "/static/media/bin.82f6028c.png"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "d42d3da9e83f3e61794ffc81fb53903f",
+    "url": "/static/media/swimmer.d42d3da9.jpg"
+  }
+]);
