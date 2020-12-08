896494fde5447ba8298d56abdbe1ff0701aa4767
Slotterony <arne.graff@gmx.de>
2020-12-08 13:49:39 +0100

    changed loginPage and css

--- my-app/src/pages/admin/AdminLogIn.js
+++ my-app/src/pages/admin/AdminLogIn.js
@@ -1,28 +1,44 @@
 import React, { Component } from 'react';
 import Button from 'react-bootstrap/Button';
 import history from './../../history';
+import { Form, Col, } from "react-bootstrap";
 
-const AdminLogIn = () => (
 
-    <React.Fragment>
-    <h1>Admin LogIn!</h1>
-    <Button variant="primary" onClick={()=>history.push({ pathname: '/admin/start'})}>Next</Button>
-    </React.Fragment>
-
-)
 
 export default class AdminLogInPage extends Component {
-
+expo
     render() {
         return(
 
             <div className="AdminLogin">
-                Bitten melden Sie sich mit Ihren Anmeldedaten ein. 
+                Bitte melden Sie sich mit Ihren Anmeldedaten an. 
+            <div>
+            <Form className="Admindaten">
+            <Form.Group controlId="formGroupId">
+              <Form.Label>Admin ID</Form.Label>
+              <Form.Control type="id" placeholder="ID eingeben" />
+            </Form.Group>
+            <Form.Group controlId="formGroupPassword">
+              <Form.Label>Passwort</Form.Label>
+              <Form.Control type="password" placeholder="Passwort" />
+            </Form.Group>
+            <Form.Group controlId="submit">
+           
+            <Button variant="primary" type="submit" className="adminLogin">
+                            Anmelden
+                    </Button>
+            </Form.Group>
+            </Form>
             </div>
+          </div>
+
+          
         );
     }
     
 };
+    
+    
     
     
     
