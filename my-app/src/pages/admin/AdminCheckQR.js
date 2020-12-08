92aefd96ef4390aad7781d70a9b4395a76b0b9c4
DoelliJ <48750590+DoelliJ@users.noreply.github.com>
2020-12-08 14:39:02 +0100

    CheckQR

--- my-app/src/pages/admin/AdminCheckQR.js
+++ my-app/src/pages/admin/AdminCheckQR.js
@@ -1,11 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import AdminNavigation from './AdminNavigation';
+import { Form, Col, } from 'react-bootstrap';
+import Button from 'react-bootstrap/Button';
 
 const AdminCheckQR = () => (
     <React.Fragment>
-         <AdminNavigation></AdminNavigation>
-    <h1>Admin Check QR Code Page!</h1>
+        <AdminNavigation></AdminNavigation>
+        <h1>Admin Check QR Code Page!</h1>
     </React.Fragment>
 )
 
-export default AdminCheckQR;
+export default class AdminCheckQRClass extends Component {
+
+    render() {
+        return (
+            <div className="CheckQR">
+                <div className="Header_QRCheck">
+                    QR Code Prüfung
+                </div>
+                <Form>
+                    <Form.Group controlId="formGroupPassword" className="QRCheckPassword">
+                        <Form.Label>QR Code:</Form.Label>
+                        <Form.Control type="password" placeholder="Password" />
+                    </Form.Group>
+
+                    <Button variant="primary" type="submit">
+                        Check
+                    </Button>
+                </Form>
+            </div>
+
+        );
+    }
+};
