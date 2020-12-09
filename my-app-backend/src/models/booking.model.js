5a88643f28b6c6dfc95f06eec9e3b520e83f4464
Dustin Ramseger <dustin.ramseger@sap.com>
2020-12-09 13:44:13 +0100

    Added scanned column

--- my-app-backend/src/models/booking.model.js
+++ my-app-backend/src/models/booking.model.js
@@ -7,6 +7,10 @@ module.exports = (sequelize, Sequelize) => {
             defaultValue: Sequelize.UUIDV4,
             allowNull: false,
             primaryKey: true
+        },
+        scanned: {
+            type: Sequelize.BOOLEAN,
+            defaultValue: false
         }
     });
 
