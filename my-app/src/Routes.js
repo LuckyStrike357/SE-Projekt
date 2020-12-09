0baf243564fa3af33e2ebb035b7dfa9f6b6dc8ee
Mona Kaczun <mona.kaczun@sap.com>
2020-12-09 21:28:28 +0100

    Token is now available on all Admin pages

--- my-app/src/Routes.js
+++ my-app/src/Routes.js
@@ -10,6 +10,7 @@ import AdminLogIn from './pages/admin/AdminLogIn';
 import AdminStart from './pages/admin/AdminStart';
 import AdminTimeslotView from './pages/admin/AdminTimeslotView';
 import AdminCheckQR from './pages/admin/AdminCheckQR';
+import AdminNavigation from './pages/admin/AdminNavigation';
 import history from './history';
 
 export default class Routes extends Component {
@@ -23,6 +24,7 @@ export default class Routes extends Component {
                     <Route path="/dataForm" component={DataFormPage} />
                     <Route path="/bookingCode" component={BookingCodePage} />
                     <Route path="/admin" exact component={AdminLogIn} />
+                    
                     <Route path="/admin/start" component={AdminStart} />
                     <Route path="/admin/timeslots" component={AdminTimeslotView} />
                     <Route path="/admin/checkQR" component={AdminCheckQR} />
--- my-app/src/pages/admin/AdminNavigation.js
+++ my-app/src/pages/admin/AdminNavigation.js
@@ -1,17 +1,23 @@
-import React, { Component }  from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import history from '../../history';
 
 
 export default class AdminNavigation extends Component {
-    
+
     state = {
         token: '',
     }
 
     componentDidMount() {
-        if(history.location.state){
-        this.setState({token:history.location.state.token});
+
+        if (history.location.state) {
+            console.log( "histroy", history.location.state.token)
+            this.setState({ token: history.location.state.token });
+            window.localStorage.setItem('token', history.location.state.token);
+        } else {
+            this.setState({ token: window.localStorage.getItem('token') });
+            console.log( "localStorage", window.localStorage.getItem('token'))
         }
     }
 
@@ -23,13 +29,13 @@ export default class AdminNavigation extends Component {
                 <nav className="adminnavigation">
                     <ul>
                         <li>
-                            <Link to="/admin/start" state={token}>Start</Link>
+                            <Link to={{pathname:"/admin/start", state:{token:token}}}>Start</Link>
                         </li>
                         <li>
-                            <Link to="/admin/timeslots" state={token}>Timeslots</Link>
+                            <Link to={{pathname:"/admin/timeslots", state:{token:token}}}>Timeslots</Link>
                         </li>
                         <li>
-                            <Link to="/admin/checkQR" state={token}>CheckQR</Link>
+                            <Link to={{pathname:"/admin/checkQR", state:{token:token}}}>CheckQR</Link>
                         </li>
                     </ul>
                 </nav>
