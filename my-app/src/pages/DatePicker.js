21bce9bca0a3ef2f2a4a1f6e47bef606df0b083e
LuckyStrike357 <dustin.ramseger@gmx.de>
2020-12-13 16:56:42 +0100

    Reusable DatePicker component

---
+++ my-app/src/pages/DatePicker.js
@@ -0,0 +1,66 @@
+import React from "react";
+import { Form, Col, } from 'react-bootstrap';
+
+const DatePicker = () => {
+
+    return (
+        <Form.Row>
+            <Form.Group as={Col} xs={3}>
+                <Form.Control required as="select" custom>
+                    <option>01</option>
+                    <option>02</option>
+                    <option>03</option>
+                    <option>04</option>
+                    <option>05</option>
+                    <option>06</option>
+                    <option>07</option>
+                    <option>08</option>
+                    <option>09</option>
+                    <option>10</option>
+                    <option>11</option>
+                    <option>12</option>
+                    <option>13</option>
+                    <option>14</option>
+                    <option>15</option>
+                    <option>16</option>
+                    <option>17</option>
+                    <option>18</option>
+                    <option>19</option>
+                    <option>20</option>
+                    <option>21</option>
+                    <option>22</option>
+                    <option>23</option>
+                    <option>24</option>
+                    <option>25</option>
+                    <option>26</option>
+                    <option>27</option>
+                    <option>28</option>
+                    <option>29</option>
+                    <option>30</option>
+                    <option>31</option>
+                </Form.Control>
+            </Form.Group>
+            <Form.Group as={Col} xs={3}>
+                <Form.Control required as="select" custom>
+                    <option>01</option>
+                    <option>02</option>
+                    <option>03</option>
+                    <option>04</option>
+                    <option>05</option>
+                    <option>06</option>
+                    <option>07</option>
+                    <option>08</option>
+                    <option>09</option>
+                    <option>10</option>
+                    <option>11</option>
+                    <option>12</option>
+                </Form.Control>
+            </Form.Group>
+            <Form.Group as={Col}>
+                <Form.Control required type="text" placeholder="Jahr" />
+            </Form.Group>
+        </Form.Row>
+    )
+}
+
+export default DatePicker;
